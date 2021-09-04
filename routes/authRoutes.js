7f7f4f950666029d26e791806254bf8a63f31135
Samarjeet Mohite <samarmohite7@gmail.com>
2021-09-04 22:32:54 +0530

    concurrently

---
+++ routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const router=express.Router();
+
+router.post('/Signup',(req,res)=>{
+console.log("request fetched");
+});
+
+module.exports=router;
--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app=express();
 const cors=require('cors');
 const morgan=require('morgan');
 const connectDB=require('./database/db');
+const authRoutes=require('./routes/authRoutes');
+
+
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use('./api/auth',authRoutes);
 
 connectDB();
 const port=process.env.PORT||8000
