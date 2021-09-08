5212ec96fc404a50a40aa352f84ac42c8f95d980
Samarjeet Mohite <samarmohite7@gmail.com>
2021-09-09 00:04:06 +0530

    dashboard set

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function AdminDashboard() {
+  const showHeader = () => {
     return (
-        <div>
-            AdminDashboard  
+      <div className="bg-dark text-white">
+        <div className="container">
+          <div className="row ">
+            <div className="col-md-6">
+              <i className="fa fa-home"> Dashboard</i>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
+  };
+  return <div>{showHeader()}</div>;
 }
--- client/src/components/UserDashboard.js
+++ client/src/components/UserDashboard.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function UserDashboard() {
+  const showHeader = () => {
     return (
-        <div>
-            InSide UserDashbotd
+      <div className="bg-dark text-white py-4">
+        <div className="container">
+          <div className="row ">
+            <div className="col-md-6">
+              <i className="fa fa-home"> Dashboard</i>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
+  };
+  return <section>{showHeader()}</section>;
 }
