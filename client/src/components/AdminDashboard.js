3b83d3b6e712973a6ef11b7c7a663405c69ca80f
Samarjeet Mohite <samarmohite7@gmail.com>
2021-09-09 17:08:58 +0530

    Buttons

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function AdminDashboard() {
   const showHeader = () => {
     return (
-      <div className="bg-dark text-white">
-        <div className="container">
+      <div className="bg-dark text-white ">
+        <div className="container align-items-center justify-content-center">
           <div className="row ">
             <div className="col-md-6">
               <i className="fa fa-home"> Dashboard</i>
@@ -14,5 +14,35 @@ export default function AdminDashboard() {
       </div>
     );
   };
-  return <div>{showHeader()}</div>;
+  const showActionBtns = () => {
+    return (
+      <div className="bg-light">
+        <div className="container">
+          <div className="row pb-3">
+            <div className="col-md-4 my-1 mw-100">
+              <button className="btn btn-outline-info btn-block">
+                <i className="fas fa-plus">Add Category</i>
+              </button>
+            </div>
+            <div className="col-md-4 my-1 mw-100  ">
+              <button className="btn btn-outline-warning btn-block">
+                <i className="fas fa-plus">Add Food Item</i>
+              </button>
+            </div>
+            <div className="col-md-4 my-1 ">
+              <button className="btn btn-outline-success btn-block">
+                <i className="fas fa-plus">View Orders</i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div>
+      {showHeader()}
+      {showActionBtns()}
+    </div>
+  );
 }
