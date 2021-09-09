8da86ea691cda0b44ccdc5324de5887d3e30d033
Samarjeet Mohite <samarmohite7@gmail.com>
2021-09-09 18:14:53 +0530

    added modal

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -20,7 +20,7 @@ export default function AdminDashboard() {
         <div className="container">
           <div className="row pb-3">
             <div className="col-md-4 my-1 mw-100">
-              <button className="btn btn-outline-info btn-block">
+              <button className="btn btn-outline-info btn-block" data-toggle="modal" data-target="#CategoryModal">
                 <i className="fas fa-plus">Add Category</i>
               </button>
             </div>
@@ -39,10 +39,54 @@ export default function AdminDashboard() {
       </div>
     );
   };
+  const showModal = () => {
+    return (
+      <div
+        className="modal fade"
+        id="CategoryModal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLongTitle">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
   return (
     <div>
       {showHeader()}
       {showActionBtns()}
+      {showModal()}
     </div>
   );
 }
