4c4386a9408ff03cea06769ac2f20bdaba22fc94
Samarjeet Mohite <samarmohite7@gmail.com>
2021-09-17 22:47:37 +0530

    loading reducer created

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,16 @@
-import {combineReducers,createStore,applyMiddleware } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import Loading from "../components/helpers/Loading";
+import loadingReducer from "./reducers/loadingReducers";
 
-const reducers=combineReducers({
-loading:Loading
+const reducers = combineReducers({
+  loading: loadingReducer,
 });
-const initialState={}
-const middleware=[thunk];
-const store=createStore(reducers,initialState,composeWithDevTools(applyMiddleware(...middleware)))
+const initialState = {};
+const middleware = [thunk];
+const store = createStore(
+  reducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 export default store;
