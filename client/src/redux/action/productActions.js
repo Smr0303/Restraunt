5a5f0a61f41145f41bf8293e9d79b36767f0e15e
Samarjeet Mohite <samarmohite7@gmail.com>
2021-09-23 22:11:37 +0530

    product action created

---
+++ client/src/redux/action/productActions.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { START_LOADING, STOP_LOADING } from "../constants/loadingConstant";
+import {
+  SHOW_SUCCESS_MESSAGE,
+  SHOW_ERROR_MESSAGE,
+} from "../constants/messageConstants";
+
+export const createProduct = (data = async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+    dispatch({ type: START_LOADING });
+    const response = await axios.post(
+      "http://localhost:5000/product/create",
+      data,
+      config
+    );
+    dispatch({ type: STOP_LOADING });
+    dispatch({
+      type: SHOW_SUCCESS_MESSAGE,
+      payload: response.data.successMessage,
+    });
+  } catch (err) {
+      dispatch({type:STOP_LOADING});
+      dispatch({type:SHOW_ERROR_MESSAGE,payload:err.response.data.errorMessage});
+  }
+});
