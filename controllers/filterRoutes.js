ef8b5ee75b602043bc1db018c5b5ea6164a8ef66
Samarjeet Mohite <samarmohite7@gmail.com>
2021-09-29 23:55:04 +0530

    Bug fix

--- controllers/filterRoutes.js
+++ controllers/filterRoutes.js
@@ -5,7 +5,10 @@ exports.getNewArrivals = async (req, res) => {
     const sortBy=req.query.sortBy?req.query.sortBy:"desc";
     const limit=req.query.limit?parseInt(req.query.limit):parseInt(3);
   try {
-      const newArrivals= await Product.findAll({})
+      const newArrivals= await Product.find({}).sort({createdAt:sortBy}).limit(limit);
+      res.status(200).json({
+          newArrivals,
+      })
   
   } catch (err) {
     console.log(err,"getnewArrivals error");
