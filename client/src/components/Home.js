6e6dbf17c0960bc6b00e06a71640324f2181f1f2
Samarjeet Mohite <samarmohite7@gmail.com>
2021-09-29 23:59:56 +0530

    frontend

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getNewArrivals } from "../redux/action/filterActions";
 export default function Home() {
-    return (
-        <div>
-         <section className="home-page">
-<div className="banner-image"></div>
-         </section>
-        </div>
-    )
+  const dispatch = useDispatch();
+  getNewArrivals();
+  useEffect(() => {
+    dispatch();
+  }, [dispatch]);
+
+  const { newArrivals } = useSelector((state) => state.filters);
+  return (
+    <div>
+      <section className="home-page">
+        <div className="banner-image"></div>
+      </section>
+    </div>
+  );
 }
