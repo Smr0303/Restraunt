e324f5ad3e33dbf7b061d6589a8b4f8aefd3295a
Samarjeet Mohite <samarmohite7@gmail.com>
2021-10-06 23:12:35 +0530

    set filter space

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 export default function Shop() {
-  return <section>
-      Inside Shop component
-      </section>;
+  return (
+    <section className="shop-page">
+      <div className="jumbotron">
+        <h1 className="display-4">Shop</h1>
+      </div>
+      <div className="row">
+        <div className="col-3 border-right ">filters</div>
+        <div className="col-3">products</div>
+      </div>
+    </section>
+  );
 }
